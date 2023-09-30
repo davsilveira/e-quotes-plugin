56cf9aea83b4d4caaf971ca9701609fdec8f7c79
Darvin da Silveira <darvin@envolvelabs.com.br>
2023-09-30 11:16:07 -0300

    Webpack working

---
+++ blocks/price/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('wp-element'), 'version' => '0af9c5554f18fc367c10');
--- webpack.config.js
+++ webpack.config.js
@@ -2,54 +2,18 @@ const defaults = require('@wordpress/scripts/config/webpack.config');
 const path = require('path');
 const glob = require('glob');
 
-module.exports = {
-	...defaults,
-	devServer: {
-		devMiddleware: { writeToDisk: true },
-		allowedHosts: 'auto',
-		host: 'equotes.local',
-		port: 8887,
-		proxy: { '/build': [Object] },
-	},
-	externals: {
-		react: 'React',
-		'react-dom': 'ReactDOM',
-	},
-	entry: () => {
-		const entries = {};
+const blockDirectories = glob.sync('./blocks/*/');
 
-		const blockFiles = glob.sync('./src/blocks/**/*.tsx'); // Alteração para .tsx
+const blockConfigs = blockDirectories.map((blockDirectory) => {
+	const blockName = path.basename(blockDirectory);
+	return {
+		...defaults,
+		entry: path.resolve(blockDirectory, 'src', 'index.js'),
+		output: {
+			filename: 'index.js',
+			path: path.resolve(blockDirectory, 'build'),
+		},
+	};
+});
 
-		blockFiles.forEach((file) => {
-			const blockName = path.basename(file, '.tsx'); // Alteração para .tsx
-			entries[blockName] = path.resolve(process.cwd(), file);
-		});
-
-		return entries;
-	},
-	output: {
-		filename: './build/[name].js', // Manter a saída como .js para compatibilidade
-		path: path.resolve(process.cwd()),
-	},
-	module: {
-		...defaults.module,
-		rules: [
-			...defaults.module.rules,
-			{
-				test: /\.tsx?$/,
-				use: [
-					{
-						loader: 'ts-loader',
-						options: {
-							configFile: 'tsconfig.json',
-							transpileOnly: true,
-						},
-					},
-				],
-			},
-		],
-	},
-	resolve: {
-		extensions: ['.ts', '.tsx', ...(defaults.resolve ? defaults.resolve.extensions || ['.js', '.jsx'] : [])],
-	},
-};
+module.exports = blockConfigs;
